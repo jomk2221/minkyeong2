ed4521d50e7c46e7f35d9c6cfd5694dea60d6aa0
JO MINKYEONG <whalsrud2221@gmail.com>
2018-04-19 11:37:33 +0000

    test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,9 @@ class ArticlesController < ApplicationController
        @article.save()
        redirect_to @article
     end
-    
+    def index
+        @articles = Article.all
+    end
     def show
       @article = Article.find(params[:id]) 
       # test comment
